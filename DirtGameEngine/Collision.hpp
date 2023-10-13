f1e851d828fe825079d57ee840f7616af5b0180a
toprak <toprakaydiner6@gmail.com>
2023-10-13 19:00:14 +0300

    lul

--- DirtGameEngine/Collision.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "Types.hpp"
-
-namespace dgm {
-	int collision(vec2* vertices1, vec2* vertices2);
-}
--- DirtGameEngine/Types.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-/// <summary>
-/// Stores custom types that i use in files across the project
-/// </summary>
-struct _vec2 { float x; float y; };
-typedef struct _vec2 vec2;
